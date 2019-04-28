d41c3428acd5fedf71ba05921ea473d981b1693d
Muhammad Maaz Tariq <mmaaztariq1997@hotmail.com>
2019-04-29 02:15:17 +0500

    Problem in crossover

--- try1.py
+++ try1.py
@@ -7,8 +7,9 @@ Created on Sun Apr 28 13:31:51 2019
 
 from itertools import permutations
 import random as rand
+import matplotlib.pyplot as plt
 
-import numpy as np
+
 
 
 cityDict ={'A': [('B', 8), ('C',10), ('D', 3), ('E', 4), ('F',6)],
@@ -18,22 +19,35 @@ cityDict ={'A': [('B', 8), ('C',10), ('D', 3), ('E', 4), ('F',6)],
         'E': [('A', 4), ('B',8), ('C', 6), ('D', 8), ('F',8)],
         'F': [('A', 6), ('B',12), ('C', 2), ('D', 11), ('E',8)]}
 
-permut=rand.sample(list(permutations('ABCDEF')), 10)
 
 def main():
-    
+    permut=rand.sample(list(permutations('ABDEF')), 10)
+    best=[]
+    average=[]
+    for i in range(100):
+        initialTourCost= fitnessFunction(permut)
+        parents=parentSelection(initialTourCost)
+        crossed= crossOver(parents)
+        mut=insertMutations(crossed)
+        tenBest= survivalSelection(mut, parents)
+        best.append(bestSoFar(tenBest))
+        average(averageBest(tenBest))
+       
+        permut=removeC(tenBest)
+    print(best)
+    print(average)
 
 def fitnessFunction(candidates):
     tourCost= []
     for individual in candidates:
-        sum=0;
+        sumd=0;
         for i in range(len(individual)-1):
             #search i, i+1 in dcit
             for j in range(len(cityDict[individual[i]])):
                 if(cityDict[individual[i]][j][0]==individual[i+1]):
-                    sum+=cityDict[individual[i]][j][1]
+                    sumd+=cityDict[individual[i]][j][1]
         
-        tourCost.append((list(individual),sum))
+        tourCost.append((list(individual),sumd))
     #print(scores)
     return tourCost
 
@@ -69,19 +83,20 @@ def parentSelection(tourCost):
     else:
         parents.append(p4[1])
         aux.remove(p4[1]) 
-    print(parents)
-    #return parents
+    #print(parents)
+    return parents
 
 def crossOver(parents):
-    sind12= rand.randint(0,5)
-    eind12=rand.randint(0,5)
-    sind34= rand.randint(0,5)
-    eind34=rand.randint(0,5)
+    sind12= rand.randint(0,4)
+    eind12=rand.randint(0,4)
+    sind34= rand.randint(0,4)
+    eind34=rand.randint(0,4)
     
-    offs1=[i for i in range(0,6)]
-    offs2=[i for i in range(0,6)]
-    offs3=[i for i in range(0,6)]
-    offs4=[i for i in range(0,6)]
+    offs1=[]
+    offs2=[]
+    offs3=[]
+    offs4=[]
+    
     
     offs1[sind12:eind12+1]=parents[0][0][sind12:eind12+1]
     offs2[sind12:eind12+1]=parents[1][0][sind12:eind12+1]
@@ -90,40 +105,40 @@ def crossOver(parents):
     
     auxind=eind12+1
     while (1):
-        if (sind12 == 0 and auxind==5):
+        if (sind12 == 0 and auxind==4):
             break
         else:
-            if(parents[1][0][auxind%6] not in offs1):
-                if(auxind%6==sind12): break
-                offs1[auxind%6]= parents[1][0][auxind%6]
+            if(parents[1][0][auxind%5] not in offs1):
+                if(auxind%5==sind12): break
+                offs1[auxind%5]= parents[1][0][auxind%5]
                 auxind+=1
         
     auxind=eind12+1
     while (1):
-        if (sind12 == 0 and auxind==5):
+        if (sind12 == 0 and auxind==4):
             break
         else:
-            if(parents[0][0][auxind%6] not in offs2):
-                if(auxind%6==sind12): break
-                offs2[auxind%6]= parents[0][0][auxind%6]
+            if(parents[0][0][auxind%5] not in offs2):
+                if(auxind%5==sind12): break
+                offs2[auxind%5]= parents[0][0][auxind%5]
                 auxind+=1
     auxind=eind34+1
     while (1):
-        if (sind34 == 0 and auxind==5):
+        if (sind34 == 0 and auxind==4):
             break
         else:
-            if(parents[3][0][auxind%6] not in offs3):
-                if(auxind%6==sind34): break
-                offs3[auxind%6]= parents[3][0][auxind%6]
+            if(parents[3][0][auxind%5] not in offs3):
+                if(auxind%5==sind34): break
+                offs3[auxind%5]= parents[3][0][auxind%5]
                 auxind+=1
     auxind=eind34+1
     while (1):
-        if (sind34 == 0 and auxind==5):
+        if (sind34 == 0 and auxind==4):
             break
         else:
-            if(parents[2][0][auxind%6] not in offs4):
-                if(auxind%6==sind34): break
-                offs4[auxind%6]= parents[2][0][auxind%6]
+            if(parents[2][0][auxind%5] not in offs4):
+                if(auxind%5==sind34): break
+                offs4[auxind%5]= parents[2][0][auxind%5]
                 auxind+=1
     crossOffsprings= [offs1,offs2, offs3, offs4]
     return crossOffsprings
@@ -136,7 +151,7 @@ def insertMutations(crossOffsprings):
             m1=rand.randint(0,5)
             m2=rand.randint(0,5)
             remv=mut.remove(mut[m2])
-            mut.insert((m1+1)%6,remv)
+            mut.insert((m1+1)%5,remv)
     else:
         return crossOffsprings
     return crossOffsprings
@@ -145,9 +160,34 @@ def survivalSelection(mutCross, parents):
     costOffspring=fitnessFunction(mutCross)
     for i in range(len(costOffspring)):
         parents.append(costOffspring[i])
-    
-    
-    
+    finalParents= addC(parents)
+    finalParents=sorted(finalParents, key= lambda x: x[2])
+    finalParents=finalParents[4:]
+    return finalParents
+
+def addC(parents):
+    c=[]
+    for i in parents:
+       c.append(i[0].insert(0, 'C')) 
+    finalParents= fitnessFunction(c)
+    return finalParents
+
+def removeC(parents):
+    c=[]
+    for i in parents:
+       c.append(i[0].remove('C')) 
+    finalParents= fitnessFunction(c)
+    return finalParents
+
+def bestSoFar(finalParents):
+    bestFitness=finalParents[-1][1]
+    return bestFitness
+
+def averageBest(finalParents):
+    sumd=0
+    for i in finalParents:
+        sumd+= i[1]
+    return sumd/6
     
 if __name__=='main':
     main()
