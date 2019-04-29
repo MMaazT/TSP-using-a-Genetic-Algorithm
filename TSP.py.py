dbc4aef7632c6001d4f9f2665a8ee96de9cc3b2e
Muhammad Maaz Tariq <mmaaztariq1997@hotmail.com>
2019-04-29 14:34:31 +0500

    changed name

--- TSP.py.py
+++ TSP.py.py
@@ -16,10 +16,8 @@ cityDict ={'A': [('B', 8), ('C',10), ('D', 3), ('E', 4), ('F',6)],
         'E': [('A', 4), ('B',8), ('C', 6), ('D', 8), ('F',8)],
         'F': [('A', 6), ('B',12), ('C', 2), ('D', 11), ('E',8)]}
 
-
 def main():
     permut=rand.sample(list(permutations('ABDEF')), 10)
-    #print(permut)
     best=[]
     average=[]
     for i in range(100):
@@ -30,30 +28,28 @@ def main():
         tenBest= survivalSelection(mut, initialTourCost)
         bo=(bestSoFar(tenBest))
         a=(averageBest(tenBest))
+        
         permut=removeC(tenBest)
+        
         best.append(bo)
         average.append(round(a,5))
-        #print(i, a)
-        #print(i, bo)
+        
     print(best)
     print(average)
-    #plt.plot(best)
-    plt.plot(average)
+    #plt.figure(figsize=(20,10))
+    #plt.show( plotBest(best))
+    plotBest(best)
+    #plotAverage(average)
     
-    
-
 def fitnessFunction(candidates):
     tourCost= []
     for individual in candidates:
         sumd=0;
         for i in range(len(individual)-1):
-            #search i, i+1 in dcit
             for j in range(len(cityDict[individual[i]])):
                 if(cityDict[individual[i]][j][0]==individual[i+1]):
-                    sumd+=cityDict[individual[i]][j][1]
-        
+                    sumd+=cityDict[individual[i]][j][1]     
         tourCost.append((list(individual),sumd))
-    #print(scores)
     return tourCost
 
 def parentSelection(tourCost):
@@ -74,6 +70,7 @@ def parentSelection(tourCost):
     else:
         parents.append(p2[1])
         aux.remove(p2[1])
+        
     p3=rand.sample(aux,2)
     if(p3[0][1]> p3[1][1]):
         parents.append(p3[0])
@@ -81,6 +78,7 @@ def parentSelection(tourCost):
     else:
         parents.append(p3[1])
         aux.remove(p3[1])
+        
     p4=rand.sample(aux,2)
     if(p4[0][1]> p4[1][1]):
         parents.append(p4[0])
@@ -88,12 +86,9 @@ def parentSelection(tourCost):
     else:
         parents.append(p4[1])
         aux.remove(p4[1]) 
-    #print(parents)
     return parents
 
 def crossOver(parents):
-
-        
     sind12= rand.randint(0,3)
     eind12=rand.randint(sind12,4)
     if(sind12==eind12):
@@ -102,37 +97,27 @@ def crossOver(parents):
     eind34=rand.randint(sind34,4)
     if(sind34==eind34):
         eind34++1
-   # print(sind12,eind12,sind34,eind34)
-    
+
     offs1=[0,0,0,0,0]
     offs2=[0,0,0,0,0]
     offs3=[0,0,0,0,0]
     offs4=[0,0,0,0,0]
     
-    
     offs1[sind12:eind12+1]=parents[0][0][sind12:eind12+1]
-    #print(offs1)
     offs2[sind12:eind12+1]=parents[1][0][sind12:eind12+1]
-    #print(offs2)
     offs3[sind34:eind34+1]=parents[2][0][sind34:eind34+1]
-    #print(offs3)
     offs4[sind34:eind34+1]=parents[3][0][sind34:eind34+1]
-    #print(offs4)
-    
-    
+       
     auxparent2=parents[1][0][eind12:]
     auxparent2= auxparent2 + parents[1][0][:eind12]
-    #print(auxparent2)
     auxind=eind12
     for j in range(len(auxparent2)):
         if(auxparent2[j] not in offs1):
             auxind+=1
             offs1[auxind%5]=auxparent2[j]
     
-    
     auxparent1=parents[0][0][eind12:]
     auxparent1= auxparent1 + parents[0][0][:eind12]
-   
     auxind=eind12
     for j in range(len(auxparent1)):
         if(auxparent1[j] not in offs2):
@@ -141,7 +126,6 @@ def crossOver(parents):
     
     auxparent4=parents[3][0][eind34:]
     auxparent4= auxparent4 + parents[3][0][:eind34]
-    
     auxind=eind34
     for j in range(len(auxparent4)):
         if(auxparent4[j] not in offs3):
@@ -150,59 +134,15 @@ def crossOver(parents):
     
     auxparent3=parents[2][0][eind34:]
     auxparent3= auxparent3 + parents[2][0][:eind34]
-   
     auxind=eind34
     for j in range(len(auxparent3)):
         if(auxparent3[j] not in offs4):
             auxind+=1
             offs4[auxind%5]=auxparent3[j]
-    """
-    auxind=eind12+1
-    par2ind=eind12+1
-    while (par2ind%5!=eind12%5+1):
-        
-        if(parents[1][0][par2ind%5] not in offs1):
-            offs1[auxind%5]= parents[1][0][par2ind%5]
-            auxind+=1
-            par2ind+=1
-        else: par2ind+=1
-        
-    auxind=eind12+1
-    par1ind=eind12+1
-    while (par1ind%5!=eind12%5+1):
-   
-        if(parents[0][0][par1ind%5] not in offs2):
-            offs2[auxind%5]= parents[0][0][par1ind%5]
-            auxind+=1
-            par1ind+=1
-        else: par1ind+=1
     
-    auxind=eind34+1
-    par4ind=eind34+1
-    while (par4ind%5!=eind34%5+1):
-    
-        if(parents[3][0][par4ind%5] not in offs3):
-            offs3[auxind%5]= parents[3][0][par4ind%5]
-            auxind+=1
-            par4ind+=1
-        else: par4ind+=1
-    
-    auxind=eind34+1
-    par3ind=eind34+1
-    while (par3ind%5!=eind34%5+1):
-        
-        if(parents[2][0][par3ind%5] not in offs4):
-            offs4[auxind%5]= parents[2][0][par3ind%5]
-            auxind+=1
-            par3ind+=1
-        else: par3ind+=1
-    """
-
     crossOffsprings= [offs1,offs2, offs3, offs4]
-    #print (crossOffsprings)
     return crossOffsprings
 
-#mutate if in probability
 def insertMutations(crossOffsprings):
     probability= round(rand.random(),2)
     if(probability<=0.20):
@@ -210,8 +150,7 @@ def insertMutations(crossOffsprings):
             m1=rand.randint(0,2)
             m2=rand.randint(3,4)
             mut.insert(m1+1,mut[m2])
-            mut.remove(mut[m2+1])
-            
+            mut.remove(mut[m2+1])          
     else:
         return crossOffsprings
     return crossOffsprings
@@ -239,7 +178,6 @@ def removeC(parents):
         i[0].remove('C') 
     for i in parents:
         c.append(i[0]) 
-    #finalParents= fitnessFunction(c)
     return c
 
 def bestSoFar(finalParents):
@@ -251,6 +189,17 @@ def averageBest(finalParents):
     for i in finalParents:
         sumd+= i[1]
     return sumd/6
+
+def plotAverage(ave):
+    plt.plot(ave)
+    plt.xlabel('Generation Number')
+    plt.ylabel('Fitness')
+    plt.title('Travelling Salesman Problem using Genetic Algorithm: Average At Each Generation')
+def plotBest(best):
+    plt.plot(best)
+    plt.xlabel('Generation Number')
+    plt.ylabel('Fitness')
+    plt.title('Travelling Salesman Problem using Genetic Algorithm: Best At Each Generation')
     
 if __name__=='main':
     main()
